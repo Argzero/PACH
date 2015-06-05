d3c1828c7aae5144c4d7b819ed813bb1e90f9c9d
Forrest Z Shooster <forrest.z.shooster@gmail.com>
2015-06-05 11:03:27 -0400

    Added bitrate and type checking

--- PMOC_Standalone/PMOC/__main__.py
+++ PMOC_Standalone/PMOC/__main__.py
@@ -1,7 +1,30 @@
 import os,sys
 import time
 from pydub import AudioSegment
-import os
+
+def _find_getch():
+    try:
+        import termios
+    except ImportError:
+        # Non-POSIX. Return msvcrt's (Windows') getch.
+        import msvcrt
+        return msvcrt.getch
+
+    # POSIX system. Create and return a getch that manipulates the tty.
+    import sys, tty
+    def _getch():
+        fd = sys.stdin.fileno()
+        old_settings = termios.tcgetattr(fd)
+        try:
+            tty.setraw(fd)
+            ch = sys.stdin.read(1)
+        finally:
+            termios.tcsetattr(fd, termios.TCSADRAIN, old_settings)
+        return ch
+
+    return _getch
+getch = _find_getch()
+
 os.system('cls' if os.name == 'nt' else 'clear')
 
 abspath = os.path.abspath(raw_input("Please provide the folder of the audio files you wish\nto convert.\n\n"))
@@ -14,54 +37,91 @@ oggs = []
 _mp3s = None
 _oggs = None 
 
-file_type = {} # 'sfx':'sfx', 'music':'music', 'voice':'voice'
+file_type = {} # sfx, sfx-r, music, voice, voice-om
 file_info = {}
 
-music = {'bitrate':128000}
-sfx_range = {'bitrate':96000}
-sfx = {'bitrate':64000}
-voice_alone = {'bitrate':96000}
-voice_over_music = {'bitrate':64000}
+music = {'bitrate':'96k'}
+sfx_range = {'bitrate':'96k'}
+sfx = {'bitrate':'64k'}
+voice_alone = {'bitrate':'96k'}
+voice_over_music = {'bitrate':'64k'}
 
 def get_bitrate(hashmap):
   return hashmap['bitrate']
-  
+def get_params(hashmap):
+  return hashmap['bitrate']
 def get_info_for_type(type):
   global music
   global sfx
   global sfx_range
   global voice_alone
   global voice_over_music
-  if(type == "music")
-    return music
-  if(type == "sfx-r")
-    return music
-  if(type == "sfx")
-    return music
-  if(type == "music")
-    return music
-    
-def infer_type(name):
-  if 'sfx' in name:
-    return 'sfx'
-  if 'music' in name:
-    return 'music'
-  if 'voice' in name:
-    return 'voice'
-  return 'unknown'
+  info = {}
+  if(type == "music"):
+    info['bitrate'] = get_bitrate(music)
+  elif(type == "sfx-r"):
+    info['bitrate'] = get_bitrate(sfx_range)
+  elif(type == "sfx"):
+    info['bitrate'] = get_bitrate(sfx)
+  elif(type == "voice"):
+    info['bitrate'] = get_bitrate(voice_alone)
+  elif(type == "voice-om"):
+    info['bitrate'] = get_bitrate(voice_over_music)
+  return info
 
+def infer_type(name):
+  global getch
+  assumed_type = "unknown"
+  if 'sfx' in name:
+    assumed_type = 'sfx'
+  elif 'music' in name:
+    assumed_type = 'music'
+  elif 'voice' in name:
+    assumed_type = 'voice'
+  if assumed_type == "unknown":
+    os.system('cls' if os.name == 'nt' else 'clear')
+    print("TYPE UNCERTAIN FOR FILE\n\n"+name+"\n\nPlease Provide the type by pressing one of the characters below:\n\n  1: Music\n  2: SFX\n  3. Voice\n\n")
+    user_input = getch()
+    if user_input == '1':
+      assumed_type = 'music'
+    elif user_input == '2':
+      assumed_type = 'sfx' 
+    elif user_input == '3':
+      assumed_type = 'voice' 
+      
+  if assumed_type == 'sfx':
+    os.system('cls' if os.name == 'nt' else 'clear')
+    print(name + "\n\n")
+    print("Does this SFX have a large range of pitches?\nPlease Provide the type by pressing one of the characters below:\n\n  1: Yes\n  2: No\n\n")
+    user_input = getch()
+    if user_input == '1':
+      assumed_type = 'sfx-r'
+    elif user_input == '2':
+      assumed_type = 'sfx' 
+  if assumed_type == 'voice':
+    os.system('cls' if os.name == 'nt' else 'clear')
+    print(name + "\n\n")
+    print("Does this voice play over music or by itself?\nPlease Provide the type by pressing one of the characters below:\n\n  1: Plays over Music\n  2: Plays Alonen\n")
+    user_input = getch()
+    if user_input == '1':
+      assumed_type = 'voice-om'
+    elif user_input == '2':
+      assumed_type = 'voice' 
+  return assumed_type
 def check_info(name):
   global file_info
   global file_type
   os.system('cls' if os.name == 'nt' else 'clear')
   if file_type[name] == 'unknown':
-    channel_count = (input("Mono?\n1: Mono\n2: Not Mono")==1)? 1: 2)
-    if channel_count == 1:
-      (file_info[name])[parameters] = ["-ac", "1"] # sets it to mono audio
-    else:
-      (file_info[name])[parameters] = []
     file_type[name] = infer_type(name)
-    
+    file_info[name] = get_info_for_type(file_type[name])
+    print("Is " + name + " Mono?\n1: Mono\n2: Not Mono");
+    user_input = getch()
+    channel_count = 1 if user_input=='1' else 2
+    if channel_count == 1:
+      (file_info[name])['parameters'] = ["-ac", "1"] # sets it to mono audio
+    else:
+      (file_info[name])['parameters'] = []
     return file_info[name]
   else:
     return file_info[name]
@@ -70,6 +130,7 @@ def wav_to_mp3(name):
   print name + "\n  : # .wav found\n  : # converting to mp3"
   global _mp3s
   global abspath
+  info = check_info(name)
   if name in _mp3s:
     print name + " MP3 EXISTS"
     return 1
@@ -77,76 +138,77 @@ def wav_to_mp3(name):
     print name + " MP3 CREATING"
     _song = AudioSegment.from_wav(abspath+"/"+name)
     _export_name = os.path.splitext(name)[0]
-    _song.export(_export_name + ".mp3", format="mp3")
+    _song.export(_export_name + ".mp3", format="mp3", bitrate=get_bitrate(info), parameters=get_params(info))
     print name + " MP3 CREATED"
     return 0
 def aiff_to_mp3(name):
   print name + "\n  : # .aiff found\n  : # converting to mp3"
   global _mp3s
   global abspath
+  info = check_info(name)
   if name in _mp3s:
      print name + " MP3 EXISTS"
      return 1
   else:
     _song = AudioSegment.from_file(abspath+"/"+name,"aac")
     _export_name = os.path.splitext(name)[0]
-    _song.export(_export_name + ".mp3", format="mp3")
+    _song.export(_export_name + ".mp3", format="mp3", bitrate=get_bitrate(info), parameters=get_params(info))
     print name + " MP3 CREATED"
-    return 0
-
-def wav_to_ogg(name):
-  print name + "\n  : # .wav found\n  : # converting to ogg"
-  global _oggs
-  global abspath
-  if name in _oggs:
-    	print name + " OGG EXISTS"
-    	return 0
-  else:
-    _song = AudioSegment.from_wav(abspath+"/"+name)
-    _export_name = os.path.splitext(name)[0]
-    _song.export(_export_name + ".ogg", format="ogg")
-    print name + " OGG CREATED"
-    return
-    
+    return 0 
 def ogg_to_mp3(name):
   print name + "\n  : # .ogg found\n  : # converting to mp3"
   global _mp3s
   global abspath
+  info = check_info(name)
   if name in _mp3s:
      print name + " MP3 EXISTS"
      return 1
   else:
     _song = AudioSegment.from_ogg(abspath+"/"+name)
     _export_name = os.path.splitext(name)[0]
-    _song.export(_export_name + ".mp3", format="mp3")
+    _song.export(_export_name + ".mp3", format="mp3", bitrate=get_bitrate(info), parameters=get_params(info))
     print name + " MP3 CREATED"
     return 0
-
-def mp3_to_ogg(name):
-  print name + "\n  : # .mp3 found\n  : # converting to ogg"
+def wav_to_ogg(name):
+  print name + "\n  : # .wav found\n  : # converting to ogg"
   global _oggs
   global abspath
+  info = check_info(name)
   if name in _oggs:
     	print name + " OGG EXISTS"
     	return 0
   else:
-    _song = AudioSegment.from_mp3(abspath+"/"+name)
+    _song = AudioSegment.from_wav(abspath+"/"+name)
     _export_name = os.path.splitext(name)[0]
-    _song.export(_export_name + ".ogg", format="ogg")
+    _song.export(_export_name + ".ogg", format="ogg", bitrate=get_bitrate(info), parameters=get_params(info))
     print name + " OGG CREATED"
-    return
-  
+    return  
 def aiff_to_ogg(name):
   print name + "\n  : # .aiff found\n  : # converting to ogg"
   global _oggs
   global abspath
+  info = check_info(name)
   if name in _oggs:
     print name + " OGG EXISTS"
     return
   else:
     _song = AudioSegment.from_file(abspath+"/"+name,"aac")
     _export_name = os.path.splitext(name)[0]
-    _song.export(_export_name + ".ogg", format="ogg")
+    _song.export(_export_name + ".ogg", format="ogg", bitrate=get_bitrate(info), parameters=get_params(info))
+    print name + " OGG CREATED"
+    return 
+def mp3_to_ogg(name):
+  print name + "\n  : # .mp3 found\n  : # converting to ogg"
+  global _oggs
+  global abspath
+  info = check_info(name)
+  if name in _oggs:
+    print name + " OGG EXISTS"
+    return 0
+  else:
+    _song = AudioSegment.from_mp3(abspath+"/"+name)
+    _export_name = os.path.splitext(name)[0]
+    _song.export(_export_name + ".ogg", format="ogg", bitrate=get_bitrate(info), parameters=get_params(info))
     print name + " OGG CREATED"
     return
     
@@ -176,18 +238,21 @@ def main():
   _oggs = set(oggs)  
   
   for wav in wavs:
-    print wav
+    os.system('cls' if os.name == 'nt' else 'clear')
     wav_to_mp3(wav)
     wav_to_ogg(wav)
   for aiff in aiffs:
+    os.system('cls' if os.name == 'nt' else 'clear')
     aiff_to_mp3(aiff)
     aiff_to_ogg(aiff)
   for mp3 in mp3s:
+    os.system('cls' if os.name == 'nt' else 'clear')
     mp3_to_ogg(mp3)
   for ogg in oggs:
+    os.system('cls' if os.name == 'nt' else 'clear')
     ogg_to_mp3(ogg)
   print "COMPLETED SUCCESSFULLY"
-  time.sleep(6)
+  time.sleep(3)
   return 0
 if __name__ == '__main__':
   main()
